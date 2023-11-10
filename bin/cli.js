ef889cee6a38e01d2946aea6bcce733d6df072b9
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-11-10 22:18:49 +0800

    优化webpack argv

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const config = require(path.resolve('webpack.config.js'))
 
-const Compiler = require('../lib/Compiler')
-const compiler = new Compiler(config)
+const webpack = require('../lib/webpack')
+const compiler = webpack(config)
 
 compiler.run()
